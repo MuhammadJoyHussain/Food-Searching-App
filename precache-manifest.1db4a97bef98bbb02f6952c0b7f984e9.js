c6bf1787285ce0639c09a28a7618a4b22b63ad78
unknown <muhammadjoyhussain@gmail.comdtrujhtyjnjjtdjhnghj>
2021-02-26 21:44:56 +0600

    Updates

---
+++ precache-manifest.1db4a97bef98bbb02f6952c0b7f984e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bd964735c5f5717adfc6e61b88636e0",
+    "url": "/Food-Searching-App/index.html"
+  },
+  {
+    "revision": "b67385323f86a9d82e0c",
+    "url": "/Food-Searching-App/static/css/main.82d7197d.chunk.css"
+  },
+  {
+    "revision": "d310b8de8575d8f79fa3",
+    "url": "/Food-Searching-App/static/js/2.dd023780.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Food-Searching-App/static/js/2.dd023780.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b67385323f86a9d82e0c",
+    "url": "/Food-Searching-App/static/js/main.c93e86d7.chunk.js"
+  },
+  {
+    "revision": "1c5d2809d2c3a5fc7c5d",
+    "url": "/Food-Searching-App/static/js/runtime-main.b92ecf51.js"
+  }
+]);
